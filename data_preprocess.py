b2be3d2791af0bcb70dee12795be6a6169dcaa52
lanhai <yuri924@163.com>
2021-02-04 15:19:57 +0800

    1. add svm 2. add adaboost 3. add neural network

--- data_preprocess.py
+++ data_preprocess.py
@@ -22,12 +22,19 @@ class MyDataset:
         self.y = np.stack(self.y).transpose()
         self.extra_x = np.stack(self.extra_x).transpose()
 
+    def get_data(self, op = 1, normalization = True):
+        if op == 1:
+            ret_x, ret_y = self.op_1st
+        else:
+            ret_x, ret_y = self.op_2nd
+        if normalization:
+            ret_x = self.data_normalize(ret_x)
+        return ret_x,ret_y
 
     @property
     def op_1st(self):
         # 149 data samples corresponding to the 149 subjects, which concat the 19 y into a single value.
         # print(np.unique(self.y))
-
         # 0对应无，1为使用，2为弃用，-1后得到 0为无弃用，1为弃用，则总的y只要有一项弃用就视为弃用
         res_y = (self.y - 1)
         res_y[np.isnan(res_y)] = 0
@@ -35,7 +42,6 @@ class MyDataset:
         # print(np.unique(res_y))
         ret_y = res_y.any(axis=1).astype(np.int8)
         ret_x = self.x
-        ret_x = self.data_normalize(ret_x)
         return ret_x,ret_y
 
     @property
@@ -53,7 +59,6 @@ class MyDataset:
         ret_y = np.concatenate(res_y)
         ret_y = (ret_y-1).astype(np.int32)
         assert ~np.any(np.isnan(ret_x))
-        ret_x = self.data_normalize(ret_x)
         return ret_x,ret_y
 
     def data_normalize(self, data):
--- main.py
+++ main.py
@@ -1,20 +1,39 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn import svm
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.neural_network import MLPClassifier
 from data_preprocess import MyDataset
 import numpy as np
 
 def main():
     d = MyDataset('./data.xls')
 
-    data_x, data_y = d.op_1st
-    # data_x, data_y = d.op_2nd
-    print(np.unique(data_y))
+    data_x, data_y = d.get_data(op=1, normalization=True)
+
     print(data_x.shape,data_y.shape)
     clf = LogisticRegression(random_state=0, solver='liblinear', max_iter=500)
     clf.fit(data_x,data_y)
-    s = clf.score(data_x,data_y)
-    print(f'The acc of logistic regression model is {s}')
+    print(f'The acc of logistic regression model is {clf.score(data_x,data_y)}')
     print(f'The weight of LR model is {clf.coef_}, the bias of LR model is {clf.intercept_}')
 
+
+    clf = svm.SVC(kernel='linear')
+    clf.fit(data_x, data_y)
+    print(f'The acc of SVM model with linear kernal is {clf.score(data_x, data_y)}')
+    print(f'The weight of SVM model is {clf.coef_}, the bias of SVM model is {clf.intercept_}')
+
+    clf = svm.SVC(kernel='rbf')
+    clf.fit(data_x, data_y)
+    print(f'The acc of SVM model with rbf kernal is {clf.score(data_x, data_y)}')
+    # print(f'The weight of SVM model is {clf.coef_}, the bias of SVM model is {clf.intercept_}')
+
+    clf = AdaBoostClassifier(n_estimators=100, random_state=0)
+    clf.fit(data_x, data_y)
+    print(f'The acc of Adaboost model is {clf.score(data_x, data_y)}')
+
+    clf = MLPClassifier(alpha=1e-5, hidden_layer_sizes = (100, 50), random_state = 1, max_iter=2000)
+    clf.fit(data_x, data_y)
+    print(f'The acc of Neural Network model is {clf.score(data_x, data_y)}')
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
